9f190d2b99610ed891d307e9d7c2091dc28fec64
Marc <marc.honegger.business@proton.me>
2024-12-07 14:31:33 +0100

    Optimisation Day 7
    2x speed, still slow :,(

--- Day07/day07.py
+++ Day07/day07.py
@@ -11,14 +11,18 @@ for line in lines:
     numbers = [int(n) for n in split_line[1].split()]
     data.append([result, numbers])
 
-def calculate_next_step_recursive(rest_numbers, operators_to_use, steps_left, goal):
-    current_operator = operators_to_use[0]
-    first, second = rest_numbers[0], rest_numbers[1]
-    new_numbers = rest_numbers[1:]
-    new_numbers[0] = int(str(first) + str(second)) if current_operator == '||' else first + second if current_operator == '+' else first * second
-    if steps_left == 0 or goal <= new_numbers[0]:
-        return new_numbers[0]
-    return calculate_next_step_recursive(new_numbers, operators[1:], steps_left - 1, goal)
+def calculate_next_step_recursive(rest_numbers, operators_to_use, interim_result, step, total_steps, goal):
+    if step == 0:
+        return calculate_next_step_recursive(rest_numbers, operators, rest_numbers[0], step + 1, total_steps, goal)
+    current_operator = operators_to_use[step - 1]
+    first, second = interim_result, rest_numbers[step]
+    current_result = int(str(first) + str(second)) if current_operator == '||' else first + second if current_operator == '+' else first * second
+    # print(first, second, current_result)
+    if step == total_steps:
+        return current_result
+    elif goal <= current_result:
+        return 0
+    return calculate_next_step_recursive(rest_numbers, operators, current_result, step + 1, total_steps, goal)
 
 possible_operators = {}
 def add_operator_recursive(current_operators, amount_left, current_level):
@@ -50,7 +54,7 @@ for l in data:
     for operators in possible_operators[len(numbers) - 2]:
         # print("Calculating: ", numbers, " to get ", result, " with ", operators)
         # print(calculate_next_step_recursive(numbers, operators))
-        if result == calculate_next_step_recursive(numbers, operators, len(numbers)-2, result):
+        if result == calculate_next_step_recursive(numbers, operators, 0, 0, len(numbers)-1, result):
             total += result
             break
 print ("TOTAL: ", total)
